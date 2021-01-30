91cff7b213bbda2c22006698e2c664b387e32062
lalitpagaria <pagaria.lalit@gmail.com>
2021-01-31 00:01:27 +0100

    Raise exception when rate limited also allow custom user agent

--- reddit_rss_reader/reader.py
+++ reddit_rss_reader/reader.py
@@ -9,6 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 from feedparser import FeedParserDict
 
+RATE_LIMIT_STR = 'you appear to be a bot and we\'ve seen too many requests'
+
 logger = logging.getLogger(__name__)
 
 
@@ -31,10 +33,16 @@ class RedditRSSReader:
     # For proper url
     # Refer https://www.reddit.com/r/pathogendavid/comments/tv8m9/pathogendavids_guide_to_rss_and_reddit/
     url: str
+    user_agent: Optional[str] = None
 
     def fetch_content(self, after: Optional[datetime] = None, since_id: Optional[int] = None) -> List[RedditContent]:
         contents: List[RedditContent] = []
-        feed = self.fetch_feed(self.url)
+        feed = self.fetch_feed(feed_url=self.url, user_agent=self.user_agent)
+
+        # Check if rate limited
+        if self.is_rate_limited(feed):
+            logger.warning("Request is rate limited, hence wait for some time before running again")
+            raise RuntimeError("Request is rate limited, hence wait for some time before running again")
 
         for entry in feed.entries:
             if after is not None and after.timetuple() > entry.updated_parsed:
@@ -44,7 +52,7 @@ class RedditRSSReader:
                 break
 
             try:
-                soup = BeautifulSoup(entry.summary)
+                soup = BeautifulSoup(entry.summary, "html.parser")
 
                 image_alt_texts = [x['alt'] for x in soup.find_all('img', alt=True)]
                 image_alt_texts = image_alt_texts if image_alt_texts else []
@@ -69,7 +77,19 @@ class RedditRSSReader:
         return contents
 
     @staticmethod
-    def fetch_feed(feed_url: str) -> FeedParserDict:
+    def is_rate_limited(feed: FeedParserDict):
+        if feed and "feed" in feed and "summary" in feed.feed:
+            text = ''.join(BeautifulSoup(feed.feed.summary, "html.parser").findAll(text=True))
+            if RATE_LIMIT_STR in text:
+                return True
+
+        return False
+
+    @staticmethod
+    def fetch_feed(feed_url: str, user_agent: Optional[str]) -> FeedParserDict:
+        if user_agent:
+            feedparser.USER_AGENT = user_agent
+
         # On MacOS https do not work, hence using workaround
         # Refer https://github.com/uvacw/inca/issues/162
         is_https = "https://" in feed_url[:len("https://")]
